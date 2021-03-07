26a1b587e60016e97474f25460f607ff3939d8a2
thackerdynasty <Thacker.dhyan@gmail.com>
2021-03-07 02:04:40 +0000

    Initial commit

---
+++ game.py
@@ -0,0 +1,18 @@
+from main import *
+print("Well,", name, ", I am thinking of a number between 1 and 20.")
+def game():
+	global Guesses
+	answer = int(input("Can you guess it? "))
+	if answer < number:
+		print("Your guess is too low.")
+		Guesses += 1
+		game()
+	elif answer > number:
+		print("Your guess is too high.")
+		Guesses += 1
+		game()
+	else:
+		if answer == number:
+			Guesses += 1
+			print("Congratulations! You guessed the number in", Guesses, "Guesses!")
+game()
---
+++ main.py
@@ -0,0 +1,5 @@
+from game import *
+import random
+Guesses = 0
+name = input("Hello! What is your name? ")
+number = random.randint(1, 20)
